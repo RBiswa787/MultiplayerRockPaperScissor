33bb47a0dd19af292615d0bd59915c2134fa3f81
Biswadeep Ray <biswa7102raydeep@gmail.com>
2022-05-18 18:05:54 +0530

    m

--- backend/index.js
+++ backend/index.js
@@ -196,6 +196,6 @@ function initialiseGame(code, name){
 //app.get('/', (req, res) => res.sendFile('index.html'));
 
 // Start the express server
-http.listen(process.env.PORT || 3000, function(){
-  console.log('listening on *:80');
+http.listen(process.env.PORT || 5000, function(){
+  console.log('listening on *:5000');
 });
